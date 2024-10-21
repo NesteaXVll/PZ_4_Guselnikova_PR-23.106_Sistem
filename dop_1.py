d1d379918c60f23c285e57c2d15d7b8376a2b95e
Anastasia <thedevil_2@mail.ru>
2024-10-21 23:28:56 +0700

    editing

--- dop_1.py
+++ dop_1.py
@@ -1,11 +1,9 @@
 filename = input("Введите имя файла: ")
 N = int(input("Введите количество строк: "))
 with open(filename, 'r') as file:
-    lines = file.readlines()
-    print(lines[1:N])
-     #line_number = 1
-     #for line in file:
-       # if line_number > N:
-         #   break
-       # print(line.rstrip())
-      #  line_number += 1
+     line_number = 1
+     for line in file:
+         if line_number > N:
+             break
+         print(line.rstrip())
+         line_number += 1
